ff5022b9e2c94abe50205815064b73f136b4c074
Lucas Murtinho <lucas.murtinho@gmail.com>
2016-06-16 20:51:28 -0300

    smartcab: Task 1 - basic (random) driving agent

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -23,19 +23,7 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # The next best move is given by the planner
-        action = self.next_waypoint
-
-        # On a red light, the agent can only turn right, and even so only if:
-        # - no oncoming traffic is going left
-        # - no traffic from the left is going forward
-        if inputs['light'] == 'red':
-            if (action != 'right') or (inputs['oncoming'] == 'left') or (inputs['left'] == 'forward'):
-                action = None
-        
-        # On a green light, the agent cannot turn left if there is
-        # oncoming traffic going forward
-        elif inputs['oncoming'] == 'forward' and action == 'left':
-            action = None
+        action = random.choice([None, 'forward', 'left', 'right'])
         
         # Execute action and get reward
         reward = self.env.act(self, action)
