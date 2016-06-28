2fac08c97e474c3c63b6f2c4c7e17116029fadb7
Lucas Murtinho <lucas.murtinho@gmail.com>
2016-06-27 22:58:52 -0300

    final version

---
+++ smartcab/run_perfect.py
@@ -0,0 +1,35 @@
+from environment import Environment
+from simulator import Simulator
+import pandas as pd
+from perfect_planner import RoutePlanner
+
+def run_sim(n_trials, agent):
+    """Run the agent for a finite number of trials."""
+
+    # Set up environment and agent
+    e = Environment()  # create environment (also adds some dummy traffic)
+    a = e.create_agent(agent)  # create agent
+    a.planner = RoutePlanner(e, a) # perfect planner
+    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
+
+    # Now simulate it
+    sim = Simulator(e, update_delay=0, display=False)  # create simulator (uses pygame when display=True, if available)
+    # NOTE: To speed up simulation, reduce update_delay and/or set display=False
+
+    return sim.run(n_trials=100)  # run for a specified number of trials
+    # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
+
+def run_sims(n_sims, n_trials, agent):
+    """
+    Run n_sims with n_trials each with the agent.
+    Returns the results as a dataframe.
+    """
+    results = []
+    for i in range(n_sims):
+        sim_results = run_sim(n_trials, agent)
+        results.append(sim_results)
+    df_results = pd.DataFrame(results)
+    df_results.columns = ['n_dest_reached', 'last_dest_fail', 'sum_time_left', 
+                          'n_penalties', 'last_penalty', 'len_qvals']
+    return df_results
